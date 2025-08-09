7cea254ccdda0afe99d74efbe59b4f84b43c33c3
Stijn Peeters <58211004+microstijn@users.noreply.github.com>
2025-08-09 22:06:49 +0200

    Removed utils solved by Git Bash

--- src/scripting.jl
+++ src/scripting.jl
@@ -8,8 +8,11 @@ using Revise
 using met2map
 
 url = "https://doi.pangaea.de/10.1594/PANGAEA.875582?format=zip"
-out = joinpath([@__DIR__, "..", "output"])
+ouf = "D:/met2map"
+out = joinpath([ouf, "output/metagenomes/wget/"])
+
+f = "D:/met2map/metagenomes/wget/ena-file-download-analysis-ERP009009-submitted_ftp-20250809-1859.sh"
 
 download_data(url, out, create_subfolder = "pang")
 
-
+execute_commands_from_file(f)
